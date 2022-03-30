cc046505367e184b3087b172b0eb363291f443f2
jaiakashvijayakumar <73417118+jaiakashvijayakumar@users.noreply.github.com>
2022-03-30 15:49:28 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+import csv
+import pandas as pd
+
+file1 = 'stars.csv'
+file2 = 'unit_stars.csv'
+
+data1 = []
+data2 = []
+
+with open(file1,'r',encoding='utf8') as f:
+
+    csv_reader =csv.reader(f)
+    
+    for i in csv_reader:
+        data1.append(i)
+        
+with open(file2,'r',encoding='utf8') as f:
+    csv_reader = csv.reader(f)
+    
+    for i in csv_reader:
+        data2.append(i)
+
+h1 = data1[0]
+h2 = data2[0]
+
+p_d1 = data1[1:]
+p_d2 = data2[1:]
+
+h = h1+h2
+
+p_d =[]
+
+for i in p_d1:
+    p_d.append(i)
+for j in p_d2:
+    p_d.append(j)
+with open("total_stars.csv",'w',encoding='utf8') as f:
+    csvwriter = csv.writer(f)
+    csvwriter.writerow(h)   
+    csvwriter.writerows(p_d)
+    
+df = pd.read_csv('total_stars.csv')
+df.tail(8)
